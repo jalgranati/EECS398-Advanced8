9a2ecca03e9544a83135947d99bd426aa757374d
Joshua Algranati <jalgrana@umich.edu>
2018-10-30 21:27:07 -0400

    send back to local

--- rpn.py
+++ rpn.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
+import readline
+import operator
+from colorama import Fore, Back, Style
+op = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.floordiv,
+}
 
 def calculate(arg):
-    stack = []
-    tokens = arg.split()
-    for token in tokens:
+    stack = arg.split()
+    while len(stack) > 1:
+        token = stack.pop()
         try:
             value = int(token)
             stack.append(value)
         except ValueError:
-            val1 = stack.pop()
-            val2 = stack.pop()
-            result = val1 + val2
-            stack.append(result)
-            return stack[0]
+            val2 = int(stack.pop())
+            val1 = int(stack.pop())
+
+            func = op[token]
+            result = func(val1,val2)
+            stack.append(str(result))
+    return int(stack[0])
 def main():
     while True:
-        print(calculate(input("rpn calc> ")))
+        result = calculate(input("rpn calc> "))
+        if result > 0:
+            print(Fore.green + str(result))
+        elif result < 0:
+            print(Fore.red + str(result))
+        else:
+            print(result)
 
 if __name__ == '__main__':
     main()
--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,16 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2,result)
+
+    def test_subtract(self):
+        result = rpn.calculate('4 3 -')
+        self.assertEqual(1,result)
+    def test_multiply(self):
+        result = rpn.calculate('4 3 *')
+        self.assertEqual(12,result)
+    def test_subtract(self):
+        result = rpn.calculate('4 2 /')
+        self.assertEqual(2,result)
+    def test_chain(seld):
+        result = rpn.calculate('1 1 + 2 *')
+        self.assertEqual(4,result)
